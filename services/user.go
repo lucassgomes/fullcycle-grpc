b3a4cc330e9673625c3e6d3cf9aa54205e152111
Lucas Soares <lucas.s.gomes@via.com.br>
2021-12-06 21:58:49 -0300

    Adiciona implementação ServerStream do lado do servidor

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/lucassgomes/fullcycle-grpc/pb/pb"
 )
@@ -25,4 +26,33 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+	time.Sleep(time.Second * 5)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "1",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 5)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "1",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+	time.Sleep(time.Second * 5)
+	return nil
 }
